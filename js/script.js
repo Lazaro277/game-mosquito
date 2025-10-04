55015dbf5df015e780b5a5d0696ceca54793b566
Lázaro <lazaromessias101@gmail.com>
2025-10-04 13:22:13 -0300

    final

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var normal = document.getElementById('normal')
+var dificil = document.getElementById('dificil')
+var chuckNorris = document.getElementById('chuckNorris')
+var iniciarJogo = document.getElementById('iniciarJogo')
+function selecionaNivel(nivel) {
+    normal.style.border = '0px solid black'
+    dificil.style.border = '0px solid black'
+    chuckNorris.style.border = '0px solid black'
+    if (nivel === 'normal') {
+        console.log('nivel normal')
+        normal.style.border = '5px solid black'
+        iniciarJogo.href = '../normal.html'
+    } else if (nivel === 'dificil') {
+        console.log('nivel dificil')
+        dificil.style.border = '5px solid black'
+        iniciarJogo.href = '../dificil.html'
+    } else if (nivel === 'chuckNorris') {
+        console.log('nivel chuckNorris')
+        chuckNorris.style.border = '5px solid black'
+        iniciarJogo.href = '../chuckNorris.html'
+    }
+}
---
+++ js/script_chuckNorris.js
@@ -0,0 +1,61 @@
+var mosca = document.getElementById('imagemMosca');
+var container = document.getElementById('jogo');
+
+function gerarPosicaoAleatoria() {
+    // Obtém as dimensões atuais do contêiner
+    const containerWidth = container.offsetWidth;
+    const containerHeight = container.offsetHeight;
+
+    // Obtém as dimensões atuais da imagem
+    const imagemWidth = mosca.offsetWidth;
+    const imagemHeight = mosca.offsetHeight;
+
+    // Calcula a posição máxima que a imagem pode ter para não sair do contêiner
+    // Subtraímos a largura/altura da imagem para que ela fique totalmente visível
+    const maxX = containerWidth - imagemWidth;
+    const maxY = containerHeight - imagemHeight;
+
+    // Garante que o maxX e maxY não sejam negativos (caso a imagem seja maior que o container)
+    const randomX = Math.max(0, Math.floor(Math.random() * maxX));
+    const randomY = Math.max(0, Math.floor(Math.random() * maxY));
+
+    // Aplica as novas posições à imagem
+    mosca.style.left = `${randomX}px`;
+    mosca.style.top = `${randomY}px`;
+}
+
+var coracao1 = document.getElementById('coracao1')
+var coracao2 = document.getElementById('coracao2')
+var coracao3 = document.getElementById('coracao3')
+var pontos = 3
+var contPontos = function contarPontos() {
+    gerarPosicaoAleatoria();
+    pontos--;
+    console.log('perdeu ponto');
+    if (pontos === 2) {
+        coracao1.src = '../img/coracao_vazio.png'
+    } else if (pontos === 1) {
+        coracao1.src = '../img/coracao_vazio.png'
+        coracao2.src = '../img/coracao_vazio.png'
+    } else if (pontos === 0) {
+        coracao1.src = '../img/coracao_vazio.png'
+        coracao2.src = '../img/coracao_vazio.png'
+        coracao3.src = '../img/coracao_vazio.png'
+    } else if (pontos === -1) {
+        window.location.href = "../game_over.html"
+    }
+}
+setInterval(contPontos, 500);
+function pontuacao() {
+    pontos++;
+    console.log('ganhou ponto')
+}
+var contagem = 30;
+setInterval(function contagemRegressiva() {
+    contagem--
+    document.getElementById('contagemRegressiva').innerHTML = contagem
+    if (contagem === 0) {
+        window.location.href = '../vitoria.html'
+    }
+}, 1000)
+
